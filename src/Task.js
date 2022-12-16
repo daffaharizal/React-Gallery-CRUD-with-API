c24388574f51741b0e2ec81d657750f8a75a95eb
daffaharizal <harizal.daffa46@gmail.com>
2022-12-16 17:36:22 +0700

    push url Netlify deploy

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL"; // TODO: replace this
+const url = "https://daffaharizal-gallery-crud-with-api.netlify.app/"; // TODO: replace this
 const studentName = "Daffa Harizal "; // TODO: replace this
 const studentId = "FE2840652"; // TODO: replace this
 
