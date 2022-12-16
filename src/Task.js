18a2b7b8c857a1a9a50cb9d1c7d237a810a5ce3f
daffaharizal <harizal.daffa46@gmail.com>
2022-12-16 17:37:45 +0700

    change name

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
 const url = "https://daffaharizal-gallery-crud-with-api.netlify.app/"; // TODO: replace this
-const studentName = "Daffa Harizal "; // TODO: replace this
+const studentName = "Daffa Harizal Fitri"; // TODO: replace this
 const studentId = "FE2840652"; // TODO: replace this
 
 export { url, studentName, studentId };
